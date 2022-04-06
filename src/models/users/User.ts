fc1f0d246135281fae32aaf57bb712cc9ffe4c3b
David Dobryakov <davidobryakov@gmail.com>
2022-04-06 17:15:00 +0300

    [feat] Add beforeCreate hook for auto generating AuthorizationLink on user creation

--- src/models/users/User.ts
+++ src/models/users/User.ts
@@ -1,3 +1,4 @@
+import { randomUUID } from 'crypto'
 import {
     Table,
     Column,
@@ -9,6 +10,7 @@ import {
     ForeignKey
 } from 'sequelize-typescript'
 import hashPassword from '../../utils/hashPassword'
+import AuthorizationLink from '../auth/AuthorizationLink'
 import Organization from '../organizations/Organization'
 import UserRole from './UserRole'
 
@@ -47,6 +49,15 @@ class User extends Model {
         if (instance.changed('password')) {
             instance.password = hashPassword(password)
         }
+    }
+
+    @BeforeCreate
+    static async generateAuthorizationLink(instance: User) {
+        const token = randomUUID()
+
+        const userId = instance.id
+
+        await AuthorizationLink.create({ token, userId })
     }
 }
 
