004adf4410333c0c5a2b91f37305d723846d64b1
David Dobryakov <davidobryakov@gmail.com>
2022-05-04 14:00:50 +0300

    [fix] Fix server faulting on getting error

--- src/controllers/cards/FilledProperty.ts
+++ src/controllers/cards/FilledProperty.ts
@@ -4,15 +4,28 @@ import FilledProperty from '../../models/cards/FilledProperty'
 class FilledPropertyController {
     getAll = async (request: any, response: any) => {
         const id = Number(request.params.cardId)
-        const card: any = await Card.findByPk(id)
 
-        const properties = await card.getProperties()
+        if (!id) {
+            return response.status(400).send({"detail": "Card ID is required param"})
+        }
 
-        return response.send(properties.map((property: FilledProperty) => property.toJSON()))
+        try {
+            const card: any = await Card.findByPk(id)
+
+            const properties = await card.getProperties()
+
+            return response.send(properties.map((property: FilledProperty) => property.toJSON()))
+        } catch (e) {
+            return response.status(404).send({"detail": "Not found"})
+        }
     }
 
     create = async (request: any, response: any) => {
         const { cardId } = request.params
+
+        if (!cardId) {
+            return response.status(400).send({"detail": "Card ID is required param"})
+        }
 
         const createdProperty = await FilledProperty.create(request.body)
 
@@ -23,6 +36,10 @@ class FilledPropertyController {
 
     delete = async (request: any, response: any) => {
         const cardId = Number(request.params.cardId)
+
+        if (!cardId) {
+            return response.status(400).send({"detail": "Card ID is required param"})
+        }
 
         const { filledPropertyId } = request.body
 
