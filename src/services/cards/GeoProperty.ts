005042b259bb78d3919373693d63d74c26b9100a
David Dobryakov <davidobryakov@gmail.com>
2022-06-08 18:42:23 +0300

    [feat] Add service for GeoProperty

---
+++ src/services/cards/GeoProperty.ts
@@ -0,0 +1,54 @@
+import GeoProperty from '../../models/cards/GeoProperty'
+
+class GeoPropertyService {
+    async getAll(): Promise<any> {
+        const geoProperties = await GeoProperty.findAll()
+
+        return {
+            detail: geoProperties,
+            status: 200
+        }
+    }
+
+    async create(propertyData: any): Promise<any> {
+        try {
+            const createdProperty = await GeoProperty.create(propertyData)
+
+            return { status: 200, detail: createdProperty }
+        } catch (e) {
+            return { status: 400, detail: e }
+        }
+    }
+
+    async getByPk(propertyId: number): Promise<any> {
+        try {
+            const property = await GeoProperty.findByPk(propertyId)
+
+            return { status: 200, detail: property }
+        } catch (e) {
+            return { status: 400, detail: e }
+        }
+    }
+
+    async update(propertyId: number, propertyData: any): Promise<any> {
+        if (propertyId > 0 && !propertyId) {
+            return { detail: 'Card id is required param', status: 400 }
+        }
+
+        try {
+            const updatedProperty: any = await GeoProperty.findByPk(propertyId)
+
+            for (const key in propertyData) {
+                updatedProperty[key] = propertyData[key]
+            }
+
+            await updatedProperty.save()
+
+            return { status: 200, detail: updatedProperty }
+        } catch (e) {
+            return { status: 400, detail: e }
+        }
+    }
+}
+
+export default GeoPropertyService
