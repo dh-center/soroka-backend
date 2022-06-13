ea21941cd5c91ff86a9b6472f5ea4ff221939fec
David Dobryakov <davidobryakov@gmail.com>
2022-06-13 22:59:01 +0300

    [ref] Make export default equals on whole project

--- src/utils/checkPassword.ts
+++ src/utils/checkPassword.ts
@@ -1,5 +1,7 @@
 import bcrypt from "bcrypt"
 
-export default (user: any, password: string) => {
+const checkPassword = (user: any, password: string) => {
     return bcrypt.compareSync(password, user.password)
 }
+
+export default checkPassword
