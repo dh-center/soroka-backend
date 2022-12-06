74930c7ea045b363c04da9e8ffbe0282a399d755
Roman <ro.kuzovlev@gmail.com>
2022-12-06 19:15:27 +0600

    remove import process

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -1,5 +1,3 @@
-import process from 'node:process'
-
 const API_PREFIX = process.env.API_PREFIX || "/restapi/v1"
 const MINIO_HOSTNAME = process.env.MINIO_HOSTNAME || 'https://soroka.f128.science'
 
